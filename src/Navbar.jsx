4556a3d4e07791342c178d26f52a5f2f958f44ca
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-08-04 21:51:16 +0500

    Gives a link to give it a star and changes app title

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
 
               <li>
                 <a
-                  href="https://www.linkedin.com/in/asharali999/"
+                  href="https://github.com/AsharAli999/Gradientoo"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Give a star
